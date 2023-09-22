c675027dc139b779abf67fa9303eac20e640e0e4
Uwe L. Korn <uwe.korn@quantco.com>
2023-09-22 17:19:24 +0200

    Attempt a Windows build

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,5 +3,6 @@
 ./configure --prefix=${PREFIX}        \
             --with-tcl=${PREFIX}/lib  \
             --with-tk=${PREFIX}/lib
+[[ "$target_platform" == "win-64" ]] && patch_libtool
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make install
