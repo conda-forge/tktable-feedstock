3785c937ef32e75a0211666b07bbc86b7af055cb
Uwe L. Korn <uwe.korn@quantco.com>
2023-09-25 13:59:22 +0200

    Debug output

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-
 set -euxo pipefail
+
+uname -s
 
 ./configure --prefix=${PREFIX}        \
             --with-tcl=${PREFIX}/lib  \
             --with-tk=${PREFIX}/lib
+
+cat config.log
+cat confdefs.h
 
 make -j${CPU_COUNT}
 make install
