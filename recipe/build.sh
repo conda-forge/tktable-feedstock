81cdd7c6e7bcc000a1e010771269dfd30666a1ce
Uwe L. Korn <uwe.korn@quantco.com>
2023-09-25 13:21:05 +0200

    Don't patch libtool

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+
+set -euxo pipefail
+
 ./configure --prefix=${PREFIX}        \
             --with-tcl=${PREFIX}/lib  \
             --with-tk=${PREFIX}/lib
-[[ "$target_platform" == "win-64" ]] && patch_libtool
+
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make install
