6cfcbdf4a5008f81e46446b4319b203d161547c2
Uwe L. Korn <uwe.korn@quantco.com>
2023-09-25 16:33:54 +0200

    CPU_COUNT=1

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -14,5 +14,8 @@ type "%LIBRARY_PREFIX%\lib\tkConfig.sh"
 
 endlocal
 
+@rem More readable error messages for debugging.
+set CPU_COUNT=1
+
 call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat
 if %ERRORLEVEL% neq 0 exit 1
