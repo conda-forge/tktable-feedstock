f2d644af794746ec5e7c9ec646c0dbf4dfcf6c60
Uwe L. Korn <uwe.korn@quantco.com>
2023-09-25 15:26:56 +0200

    Remove debug output

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,13 +2,9 @@
 
 set -euxo pipefail
 
-uname -s
-
 ./configure --prefix=${PREFIX}        \
             --with-tcl=${PREFIX}/lib  \
             --with-tk=${PREFIX}/lib
-
-cat config.log
 
 make -j${CPU_COUNT}
 make install
